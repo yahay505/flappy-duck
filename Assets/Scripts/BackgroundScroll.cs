3a7bdf60e5881090f0c6246081b694bf63d464cf
yavuz <yavuz.celik.05@gmail.com>
2020-11-29 20:44:45 +0300

    added screen scroll

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+    public float ScroolSpeed;
+    public float TeleportPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * ScroolSpeed * Time.deltaTime;
+        if (transform.position.x<=TeleportPoint)
+        {
+            Debug.Log("tped");
+            transform.position=new Vector3(0, transform.position.y, transform.position.z);
+        }
+    }
+}
